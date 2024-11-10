74bb6396577c8241f7e6bb4e17cce498f206b114
alex <alexandre.ag.67@gmail.com>
2024-11-10 16:21:50 +0100

    Implémentation de l'interface Fly dans la classe Eagle.

--- Eagle.java
+++ Eagle.java
@@ -1,6 +1,6 @@
-public class Eagle extends Bird {
+public class Eagle extends Bird implements Fly{
 
-    private boolean flying;
+    private final boolean flying;
     private int altitude;
 
     public Eagle(String name) {
@@ -9,16 +9,45 @@ public class Eagle extends Bird {
         this.altitude = 0;
     }
 
-    public int getAltitude() {
-        return altitude;
-    }
-
-    public boolean isFlying() {
-        return flying;
-    }
-
     @Override
     public String sing() {
         return "Screech!";
+    }
+
+    @Override
+    public void takeOff() {
+        if (this.altitude == 0) {
+            System.out.println(this.getName() + " takes off in the sky.");
+        }
+    }
+
+    @Override
+    public void ascend(int meters) {
+        this.altitude += meters;
+        System.out.println(this.getName() + " flies upward, altitude: " + this.altitude);
+    }
+
+    @Override
+    public void descend(int meters) {
+        if (this.altitude - meters < 0) {
+            this.altitude = 0;
+        } else {
+            this.altitude -= meters;
+        }
+        System.out.println(this.getName() + " flies downward, altitude: " + this.altitude);
+    }
+
+    @Override
+    public void land() {
+        if (this.altitude > 1) {
+            System.out.println(this.getName() + " is too high, it can't land.");
+        } else {
+            System.out.println(this.getName() + " lands on the ground.");
+        }
+    }
+
+    @Override
+    public void glide() {
+        System.out.println(this.getName() + " glides into the air.");
     }
 }
